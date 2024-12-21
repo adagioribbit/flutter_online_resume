78a24eb37a7d67e9ffc6673718b253f79fe66565
Benjamin Israël <pas2mail@pas1rond.org>
2024-12-21 18:41:34 +0100

    Partial manilla folder

--- lib/main.dart
+++ lib/main.dart
@@ -49,51 +49,70 @@ class _CardAndTabState extends State<CardAndTab> {
     final double screenHeight = MediaQuery.sizeOf(context).height;
     final double screenWidth = MediaQuery.sizeOf(context).width;
 
-    return Flexible(
-      flex: 1,
-      fit: FlexFit.tight,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Stack(
-            alignment: AlignmentDirectional.bottomStart,
-            children: [
-              Container(
-                width: 150,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(12.0),
-                ),
-                constraints: BoxConstraints(
-                  minHeight: 500,
-                  minWidth: 50,
-                ),
-                margin: EdgeInsetsDirectional.fromSTEB(50, 50, 50, 50),
+    return
+    Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Container(
+              width: 150,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                color: Color(0xffceb88f),
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(12.0), topRight: Radius.circular(12.0)),
               ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(12.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey,
-                      spreadRadius: 0,
-                      blurRadius: 15,
-                      offset: Offset(5, 12), // changes position of shadow
-                    ),
+              constraints: BoxConstraints(
+                minHeight: 45,
+                minWidth: 50,
+              ),
+              margin: EdgeInsetsDirectional.fromSTEB(50, 0, 0, 0),
+            ),
+            Container(
+              width: 15,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                gradient: RadialGradient(
+                  stops: [0, 0.2, 0.2, 1],
+                  colors: [
+                    const Color.fromRGBO(254, 247, 255, 1),
+                    const Color.fromRGBO(254, 247, 255, 1),
+                    Color(0xffceb88f),
+                    Color(0xffceb88f),
                   ],
+                  center: Alignment.topRight,
+                  radius: 5,
                 ),
-                constraints: BoxConstraints(
-                  minHeight: 450,
-                  minWidth: 250,
-                ),
-                margin: EdgeInsetsDirectional.fromSTEB(50, 50, 50, 50),
               ),
-            ]
+              constraints: BoxConstraints(
+                minHeight: 15,
+              ),
+            ),
+          ],
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Color(0xffceb88f),
+            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(12.0), bottomRight: Radius.circular(12.0), topRight: Radius.circular(12.0)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                spreadRadius: 0,
+                blurRadius: 15,
+                offset: Offset(5, 12), // changes position of shadow
+              ),
+            ],
           ),
-        ],
-      )
+          constraints: BoxConstraints(
+            minHeight: 450,
+            minWidth: 250,
+          ),
+          margin: EdgeInsetsDirectional.fromSTEB(50, 0, 50, 50),
+        ),
+      ]
     );
   }
 }
