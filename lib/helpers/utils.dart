e0b44074dd3ef460a31ed063841a8796cd6d1886
Benjamin Israël <pas2mail@pas1rond.org>
2025-01-14 13:50:42 +0100

    Add Utils.isFoldable method

--- lib/helpers/utils.dart
+++ lib/helpers/utils.dart
@@ -11,6 +11,13 @@ class Utils {
   }
 
   static bool isPhoneScreen(BuildContext context) {
-    return MediaQuery.of(context).size.width < 768;
+    bool isPhoneScreen = MediaQuery.of(context).size.width < 768 &&
+        MediaQuery.of(context).size.width >= 360;
+    return isPhoneScreen;
+  }
+
+  static bool isFoldable(BuildContext context) {
+    bool isFoldable = MediaQuery.of(context).size.width < 360;
+    return isFoldable;
   }
 }
